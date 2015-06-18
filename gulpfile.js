4579010d9b72425dae29f04c996e7dc4951a2dae
Alexandr Kleshch <markelov2day@gmail.com>
2015-06-18 08:38:55 +0300

    Modified gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,20 @@
 'use strict';
+var gulp = require('gulp'),
+    jade = require('jade');
+
+// Default
+gulp.task('default', ['template']);
+
+// Watch
+gulp.task('watch', function() {
+    gulp.watch(['./src'], ['default'])
+});
+
+// Template
+gulp.task('template', function () {
+    gulp.src('./src/template/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist'));
+});
